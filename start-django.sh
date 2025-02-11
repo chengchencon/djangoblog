f6a4e7b98c1c9cbfb0e3d9bb3e2c62d34ef211bd
CHENG CHEN <105932025+chengchencon@users.noreply.github.com>
2025-02-10 19:05:43 -0500

    Update start-django.sh

--- start-django.sh
+++ start-django.sh
@@ -4,7 +4,7 @@ poetry run python manage.py collectstatic --no-input
 poetry run python manage.py migrate
 
 if [[ "$ENV_STATE" == "production" ]]; then
-  poetry run gunicorn djangoblog.wsgi --workers $GUNICORN_WORKERS --forwarded-allow-ips "*"
+  poetry run gunicorn djangoblogsite.wsgi --workers $GUNICORN_WORKERS --forwarded-allow-ips "*"
 else
   poetry run python manage.py runserver 0.0.0.0:8000
 fi
